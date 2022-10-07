67e465ff6d4f1defdfc894aa8202d7df2b0ada26
Zeju1997 <zqiu@login2.is.localnet>
2022-10-07 18:49:33 +0200

    update

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 # from trainer_blackbox_mixup_rl import Trainer
 # from trainer_blackbox_implicit_cnn import Trainer
-from trainer_blackbox_mixup_rl import Trainer
+from trainer_blackbox_implicit_cnn import Trainer
 
 from options.options import Options
 import os
