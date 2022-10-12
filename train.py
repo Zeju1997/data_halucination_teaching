6838306a258d0dcc5170d2cb1ed727d144f7728a
Zeju1997 <zeju.qiu@gmail.com>
2022-10-12 19:37:22 +0200

    update

--- train.py
+++ train.py
@@ -78,8 +78,8 @@ if __name__ == "__main__":
     options = Options()
     opts = options.parse()
 
-    config = load_config("mnist_blackbox_implicit.yaml")
-    # config = load_config("cifar10.yaml")
+    # config = load_config("mnist_blackbox_implicit.yaml")
+    config = load_config("cifar100.yaml")
     # config = load_config("cifar100.yaml")
 
     opts.set_defaults(**config)
