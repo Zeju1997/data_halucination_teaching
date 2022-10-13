4bc6d7ec4ff6c75691f6544184c3e1d3585e95c1
Zeju1997 <zeju.qiu@gmail.com>
2022-10-13 05:09:32 +0200

    update blackbox mixup rl

--- train.py
+++ train.py
@@ -83,8 +83,8 @@ if __name__ == "__main__":
     options = Options()
     opts = options.parse()
 
-    # config = load_config("mnist_blackbox_mixup.yaml")
-    config = load_config("cifar10_blackbox_mixup.yaml")
+    config = load_config("mnist_blackbox_mixup.yaml")
+    # config = load_config("cifar10_blackbox_mixup.yaml")
     # config = load_config("cifar100.yaml")
 
     opts.set_defaults(**config)
